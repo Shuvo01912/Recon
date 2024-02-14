6b725198f0d8de0da10c6b2dd022889c1fa5b4e9
Shuvo Kumar Saha <65779943+syper-shuvo@users.noreply.github.com>
2024-02-14 22:03:39 +0600

    Update and rename subdomain.txt to subdomain.sh

--- subdomain.sh
+++ subdomain.sh
@@ -7,10 +7,9 @@ subfinder -d $1 -silent | tee -a $1/subs.txt
 assetfinder -subs-only $1 | tee -a $1/subs.txt
 amass enum -r 0.0.0.0 -passive -d $1 | tee -a $1/subs.txt
                                                                                                                                                                                                        cat $1/subs.txt | httpx -silent | tee -a $1/alive.txt              
-# nmap (nmap -sC -sV Ip)
 
-# nikto tool use here
+cat $1/subs.txt | xargs -I @ dig @ | grep -E 'CNAME'
 
 ## test for nuclei 
 
-cat $1/alive.txt | nuclei -t /home/shuvo/nuclei-templates -es info,unknown -etags ssl,network | tee -a $1/nuclei.txt
+#cat $1/alive.txt | nuclei -t /home/shuvo/nuclei-templates -es info,unknown -etags ssl,network | tee -a $1/nuclei.txt
